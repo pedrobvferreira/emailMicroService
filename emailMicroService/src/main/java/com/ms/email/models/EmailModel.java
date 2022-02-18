c201319bbac61c541e2ee1e7a43d8d0ea8e984ca
pedrobvferreira <pedrobvf@hotmail.com>
2022-02-18 17:51:54 +0000

    Create Microservice to Send emails

---
+++ emailMicroService/src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,98 @@
+package com.ms.email.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.ms.email.enums.StatusEmail;
+
+@Entity
+@Table(name = "email_model")
+public class EmailModel implements Serializable {
+
+	private static final long serialVersionUID = -3013122315662714947L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private String reference;
+	private String emailFrom;
+	private String emailTo;
+	private String subject;
+	@Column(columnDefinition = "TEXT")
+	private String text;
+	private LocalDateTime sendDate;
+	private StatusEmail statusEmail;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getSendDate() {
+		return sendDate;
+	}
+
+	public void setSendDate(LocalDateTime sendDate) {
+		this.sendDate = sendDate;
+	}
+
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
+
+}
---
+++ emailMicroService/src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,55 @@
+package com.ms.email.services;
+
+import java.awt.print.Pageable;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+
+@Service
+public class EmailService {
+
+	@Autowired
+    EmailRepository emailRepository;
+	
+	@Autowired
+    private JavaMailSender emailSender;
+
+	/** Method to send Email **/
+	public EmailModel sendEmail(EmailModel emailModel) {
+		emailModel.setSendDate(LocalDateTime.now());
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(emailModel.getEmailFrom());
+			message.setTo(emailModel.getEmailTo());
+			message.setSubject(emailModel.getSubject());
+			message.setText(emailModel.getText());
+			emailSender.send(message);
+			
+			emailModel.setStatusEmail(StatusEmail.SENT);
+		} catch (MailException e) {
+			emailModel.setStatusEmail(StatusEmail.ERROR);
+		} finally {
+			return emailRepository.save(emailModel);
+        }
+	}
+	
+	public Page<EmailModel> findAll(Pageable pageable) {
+		return null;
+//        return emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId) {
+        return emailRepository.findById(emailId);
+    }
+}
