0d2ce2287e424ccbc0f10c6c873e2b657b48b17d
pedrobvferreira <pedrobvf@hotmail.com>
2022-02-18 17:01:07 +0000

    Create Spring Microservice

---
+++ emailMicroService/src/main/java/com/ms/email/MSEmailApp.java
@@ -0,0 +1,12 @@
+package com.ms.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSEmailApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MSEmailApp.class, args);
+	}
+}
