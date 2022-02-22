544644cb4b9fdc11204d5202b1401e019f53cceb
pedrobvferreira <pedrobvf@hotmail.com>
2022-02-22 21:18:20 +0000

    fix

--- userMicroService/src/main/java/com/ms/user/controllers/UserController.java
+++ userMicroService/src/main/java/com/ms/user/controllers/UserController.java
@@ -3,15 +3,12 @@ package com.ms.user.controllers;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PagedResourcesAssembler;
