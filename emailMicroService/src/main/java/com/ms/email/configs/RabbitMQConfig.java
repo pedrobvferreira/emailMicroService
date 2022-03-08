a4b1b231db00ef28a6a0cfb22a4dd66be177b450
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-08 21:43:00 +0000

    fix

--- emailMicroService/src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ emailMicroService/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 
 @Configuration
 public class RabbitMQConfig {
@@ -19,7 +20,7 @@ public class RabbitMQConfig {
 	}
 	
 	@Bean
-    public Jackson2JsonMessageConverter messageConverter() {
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 }
