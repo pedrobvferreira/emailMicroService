fe6d77287689a26069c5d6b568259590766a79b3
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-22 15:23:53 +0000

    eureka client

--- userMicroService/src/main/java/com/ms/user/MSUserApp.java
+++ userMicroService/src/main/java/com/ms/user/MSUserApp.java
@@ -2,11 +2,13 @@ package com.ms.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Pedro Ferreira
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MSUserApp {
 	
 	public static void main(String[] args) {
