42b93aaa3723fad89295edc66359a13df16c32cc
pedrobvferreira <pedrobvf@hotmail.com>
2022-03-22 15:27:29 +0000

    eureka client

--- emailMicroService/src/main/java/com/ms/email/MSEmailApp.java
+++ emailMicroService/src/main/java/com/ms/email/MSEmailApp.java
@@ -2,11 +2,13 @@ package com.ms.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Pedro Ferreira
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MSEmailApp {
 	
 	public static void main(String[] args) {
